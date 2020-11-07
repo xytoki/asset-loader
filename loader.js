1975b3d4d722b89393d09be2d31b12e55f67e972
xytoki <7547189+xytoki@users.noreply.github.com>
2020-11-07 22:55:35 +0800

    use npm name to allow reusing runtime

--- loader.js
+++ loader.js
@@ -1,11 +1,8 @@
-const path = require("path")
-const store = JSON.stringify(path.join(__dirname,"runtime.js"));
-
 module.exports = function () { };
 module.exports.pitch = function (remainingRequest) {
     this.cacheable();
     return [
         `var url=require(${JSON.stringify("-!" + remainingRequest)});`,
-        `module.exports = typeof(window)=="undefined"?url:require(${store}).push(url);`
+        `module.exports = typeof(window)=="undefined"?url:require('@xytoki/asset-loader').push(url);`
     ].join("\r\n")
 };
