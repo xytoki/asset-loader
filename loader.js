a0383e04ee898b7a8be67859477f0bc563449cc1
xiaoyu <7547189+xytoki@users.noreply.github.com>
2022-01-06 22:26:59 +0800

    ✨ feat: use resourcequery to support webpack 5

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@ module.exports = function () { };
 module.exports.pitch = function (remainingRequest) {
     this.cacheable();
     return [
-        `var url=require(${JSON.stringify("-!" + remainingRequest)});`,
+        `var url=require(${JSON.stringify("-!" + remainingRequest + "?asset-loader-raw")});`,
         `module.exports = typeof(window)=="undefined"?url:require('@xytoki/asset-loader').push(url);`
     ].join("\r\n")
 };
